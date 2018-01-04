fee5246f79d56785857be1ad4966af3f832eca93
Minwoo Jeong <jmw95217@gmail.com>
2018-01-04 15:23:54 +0900

    add vcfdata class

---
+++ vcf/vcfdata.py
@@ -0,0 +1,101 @@
+import sys, os
+
+class VCFData:
+    """ from annovar, only consider SNV """
+    def __init__(self):
+        self.chrID = ''
+        self.pos = 0  # 1-base
+        self.dbSNP_id = ''
+        self.ref_nuc = ''
+        self.alt_nuc = ''
+        self.qual = 0.0
+        self.filter = ''
+        self.info = ''
+        self.format = ''
+
+        """ info from annovar """
+        self.anno_gene_sym = ''
+        self.anno_genic = ''
+        self.anno_alt_genic = ''
+        self.anno_SNV_type = ''
+
+        """ info from refSeq (in-house rep-isoforms) """
+        self.gene_sym_to_genic = {}
+        self.rep_gene_sym = ''
+        self.rep_genic = ''
+
+    # END: __init__
+
+    @staticmethod
+    def parse_vcf_file(vcf_filename, isClustered=False):
+        alt_genic_dict = {'exonic': 'exonic', 'UTR3': '3UTR', 'UTR5': '5UTR', 'intronic': 'intronic',
+                          'splicing': 'intronic', 'ncRNA_splicing': 'ncRNA_intronic', 'downstream': 'intergenic',
+                          'ncRNA_exonic': 'ncRNA_exonic', 'intergenic': 'intergenic', 'ncRNA_UTR5': 'ncRNA_exonic',
+                          'ncRNA_UTR3': 'ncRNA_exonic', 'upstream': 'intergenic', 'ncRNA_intronic': 'ncRNA_intronic',
+                          '5UTR': '5UTR', '3UTR':'3UTR', 'other':'other'}
+
+        if not os.path.isfile(vcf_filename):
+            sys.exit('File Not Found %s' % vcf_filename)
+
+        var_list = []
+        vcf_file = open(vcf_filename, 'r')
+
+        for line in vcf_file:
+            # File Format
+            # Column Number:     | 0       | 1        | 2          | 3       | 4
+            # Column Description:| chrID   | pos      | dbSNP_id   | ref_nuc | alt_nuc
+            # Column Example:    | chr13   | 32906558 | rs79483201 | T       | A
+            # Column Number:     | 5       | 6        | 7          | 8              | 9./..
+            # Column Description:| qual    | filter  | info        | format         | SampleIDs
+            # Column Example:    | 5645.6  | PASS     | .          | GT:AD:DP:GQ:PL | Scores corresponding to format
+            ##FORMAT=<ID=AD,Number=.,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
+            ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth (reads with MQ=255 or with bad mates are filtered)">
+            ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+            ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+            ##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Normalized, Ph
+
+            if line.startswith('#'): continue  # SKIP information Headers
+            fields = line.strip('\n').split('\t')
+
+            ## filter for chrID
+            if fields[0] == 'MT':
+                continue
+            if fields[0].startswith('<GL00'):
+                continue
+            if fields[0].startswith('.'):
+                continue
+
+            ## check if the variant passed the variant calling filters
+            if 'PASS' not in line:
+                continue
+
+            variant = VCFData()
+            variant.chrID  = 'chr%s' % fields[0]
+
+            try: variant.pos = int(fields[1])
+            except ValueError: continue
+
+            variant.dbSNP_id = fields[2]
+            variant.ref_nuc = fields[3]
+            variant.alt_nuc = fields[4]
+            variant.qual = float(fields[5]) if fields[5] != '.' else fields[5]
+            variant.filter = fields[6]
+            variant.info = fields[7]
+            variant.format = fields[8]
+            variant.list_sSamples = fields[9:]
+
+            vcf_info_dict = dict(info.split('=') for info in variant.info.split(';')[:-1] if len(info.split('=')) == 2)
+
+            variant.anno_gene_sym = vcf_info_dict['Gene.refGene'].split('\\x3b')[0]
+            variant.anno_genic = vcf_info_dict['Func.refGene'].split('\\x3b')[0]
+            variant.anno_alt_genic = alt_genic_dict[vcf_info_dict['Func.refGene'].split('\\x3b')[0]]
+            variant.anno_SNV_type = vcf_info_dict['ExonicFunc.refGene'].split('\\x3b')[0]
+
+            var_list.append(variant)
+        # END: for loop 'line'
+
+        vcf_file.close()
+
+        return var_list
+    # END: parse_vcf_file
+# END: class 'VCFData'
