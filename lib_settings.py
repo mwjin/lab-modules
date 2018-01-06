f3b04cff36b837405c01a1eacb37546f196ee1ac
Minwoo Jeong <jmw95217@gmail.com>
2018-01-06 11:10:15 +0900

    add ROOT_DIR and commants

--- lib_settings.py
+++ lib_settings.py
@@ -2,5 +2,6 @@
 
 from os.path import dirname, abspath
 
-BASE_DIR = dirname(dirname(abspath(__file__)))
-DATA_DIR = "%s/data" % BASE_DIR
+BASE_DIR = dirname(dirname(abspath(__file__)))  # the directory that contains all my projects
+DATA_DIR = "%s/data" % BASE_DIR  # the directory that contains common data for my projects
+ROOT_DIR = dirname(BASE_DIR)
