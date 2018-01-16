b7ca2852ff439eb09617c82aad27397f72a948b6
Minwoo Jeong <jmw95217@gmail.com>
2018-01-16 18:27:22 +0900

    add lib_utils

--- lib_settings.py
+++ lib_settings.py
@@ -1,7 +1,8 @@
-""" Definitions for my library """
+""" Settings for all the projects """
 
 from os.path import dirname, abspath
 
+# paths for all the projects
 BASE_DIR = dirname(dirname(abspath(__file__)))  # the directory that contains all my projects
 DATA_DIR = "%s/data" % BASE_DIR  # the directory that contains common data for my projects
 ROOT_DIR = dirname(BASE_DIR)
