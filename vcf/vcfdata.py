5aa56b4d538814941f7c075addcd701a0c5357b3
Minwoo Jeong <jmw95217@gmail.com>
2018-01-16 10:44:07 +0900

    update vcfdata (remove the codes associated with ANNOVER)

--- vcf/vcfdata.py
+++ vcf/vcfdata.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, re
 
 class VCFData:
     """ The object of this class represents one entry in VCF files (only consider SNV). """
@@ -12,20 +12,91 @@ class VCFData:
         self.filter = ''
         self.info = ''
         self.format = ''
-
-        """ info from annovar """
-        self.anno_gene_sym = ''
-        self.anno_genic = ''
-        self.anno_alt_genic = ''
-        self.anno_SNV_type = ''
+        self.samples = []
 
         """ info from refFlat (in-house rep-isoforms) """
         self.genic_region_dict = {}  # Mapping route: gene symbol -> ID -> genic region
         self.rep_gene_sym = None
         self.rep_gene_id = None
-        self.rep_genic = 'intergenic'
+        self.rep_genic = 'intergenic'  # default
 
     # END: __init__
+
+    @staticmethod
+    def parse_vcf_file(vcf_filename, isClustered=False):
+
+        if not os.path.isfile(vcf_filename):
+            sys.exit('File Not Found %s' % vcf_filename)
+
+        # regex for VCF entries
+        p_autosome = re.compile('^[0-9]{1,2}$')
+        p_sexchr = re.compile('^[XY]$')
+        p_pos = re.compile('^[0-9]+$')
+
+        var_list = []
+        vcf_file = open(vcf_filename, 'r')
+
+        for line in vcf_file:
+            # File Format
+            # Column Number:     | 0       | 1        | 2          | 3       | 4
+            # Column Description:| chrID   | pos      | dbSNP_id   | ref_nuc | alt_nuc
+            # Column Example:    | 13      | 32906558 | rs79483201 | T       | A
+            # Column Number:     | 5       | 6        | 7          | 8              | 9./..
+            # Column Description:| qual    | filter   | info       | format         | SampleIDs
+            # Column Example:    | 5645.6  | PASS     | .          | GT:AD:DP:GQ:PL | Scores corresponding to format
+
+            # Examples of the formats
+            ##FORMAT=<ID=AD,Number=.,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
+            ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth (reads with MQ=255 or with bad mates are filtered)">
+            ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+            ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+            ##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Normalized, Ph
+
+            if line.startswith('#'):  # skip information headers
+                continue
+
+            ## check if the variant passed the variant calling filters
+            if 'PASS' not in line:
+                continue
+
+            fields = line.strip('\n').split('\t')
+
+            chrID = fields[0]
+
+            ## filters for chrID
+            if p_autosome.match(chrID):
+                if int(chrID) > 23:  # Wrong chromosome number
+                    print('Invalid chromosome ID in \'%s\'' % line)
+                    continue
+
+            elif not p_sexchr.match(chrID):
+                print('Invalid chromosome ID in \'%s\'' % line)
+                continue
+
+            variant = VCFData()
+            variant.chrID  = 'chr%s' % fields[0]
+
+            if not p_pos.match(fields[1]):
+                print('Invalid variant position in \'%s\'' % line)
+                continue
+
+            variant.pos = int(fields[1])
+            variant.dbSNP_id = fields[2]
+            variant.ref_nuc = fields[3]
+            variant.alt_nuc = fields[4]
+            variant.qual = float(fields[5]) if fields[5] != '.' else fields[5]
+            variant.filter = fields[6]
+            variant.info = fields[7]
+            variant.format = fields[8]
+            variant.samples = fields[9:]
+
+            var_list.append(variant)
+        # END: for loop 'line'
+
+        vcf_file.close()
+
+        return var_list
+    # END: parse_vcf_file
 
     def set_genic_region(self, genes_same_chr):
         """
@@ -89,77 +160,4 @@ class VCFData:
             # END: for loop 'gene_id'
         # END: for loop 'gene_sym'
     # END: _set_rep_genic_region
-
-    @staticmethod
-    def parse_vcf_file(vcf_filename, isClustered=False):
-        alt_genic_dict = {'exonic': 'exonic', 'UTR3': '3UTR', 'UTR5': '5UTR', 'intronic': 'intronic',
-                          'splicing': 'intronic', 'ncRNA_splicing': 'ncRNA_intronic', 'downstream': 'intergenic',
-                          'ncRNA_exonic': 'ncRNA_exonic', 'intergenic': 'intergenic', 'ncRNA_UTR5': 'ncRNA_exonic',
-                          'ncRNA_UTR3': 'ncRNA_exonic', 'upstream': 'intergenic', 'ncRNA_intronic': 'ncRNA_intronic',
-                          '5UTR': '5UTR', '3UTR':'3UTR', 'other':'other'}
-
-        if not os.path.isfile(vcf_filename):
-            sys.exit('File Not Found %s' % vcf_filename)
-
-        var_list = []
-        vcf_file = open(vcf_filename, 'r')
-
-        for line in vcf_file:
-            # File Format
-            # Column Number:     | 0       | 1        | 2          | 3       | 4
-            # Column Description:| chrID   | pos      | dbSNP_id   | ref_nuc | alt_nuc
-            # Column Example:    | chr13   | 32906558 | rs79483201 | T       | A
-            # Column Number:     | 5       | 6        | 7          | 8              | 9./..
-            # Column Description:| qual    | filter  | info        | format         | SampleIDs
-            # Column Example:    | 5645.6  | PASS     | .          | GT:AD:DP:GQ:PL | Scores corresponding to format
-            ##FORMAT=<ID=AD,Number=.,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
-            ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth (reads with MQ=255 or with bad mates are filtered)">
-            ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
-            ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
-            ##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Normalized, Ph
-
-            if line.startswith('#'): continue  # SKIP information Headers
-            fields = line.strip('\n').split('\t')
-
-            ## filter for chrID
-            if fields[0] == 'MT':
-                continue
-            if fields[0].startswith('<GL00'):
-                continue
-            if fields[0].startswith('.'):
-                continue
-
-            ## check if the variant passed the variant calling filters
-            if 'PASS' not in line:
-                continue
-
-            variant = VCFData()
-            variant.chrID  = 'chr%s' % fields[0]
-
-            try: variant.pos = int(fields[1])
-            except ValueError: continue
-
-            variant.dbSNP_id = fields[2]
-            variant.ref_nuc = fields[3]
-            variant.alt_nuc = fields[4]
-            variant.qual = float(fields[5]) if fields[5] != '.' else fields[5]
-            variant.filter = fields[6]
-            variant.info = fields[7]
-            variant.format = fields[8]
-            variant.list_sSamples = fields[9:]
-
-            vcf_info_dict = dict(info.split('=') for info in variant.info.split(';')[:-1] if len(info.split('=')) == 2)
-
-            variant.anno_gene_sym = vcf_info_dict['Gene.refGene'].split('\\x3b')[0]
-            variant.anno_genic = vcf_info_dict['Func.refGene'].split('\\x3b')[0]
-            variant.anno_alt_genic = alt_genic_dict[vcf_info_dict['Func.refGene'].split('\\x3b')[0]]
-            variant.anno_SNV_type = vcf_info_dict['ExonicFunc.refGene'].split('\\x3b')[0]
-
-            var_list.append(variant)
-        # END: for loop 'line'
-
-        vcf_file.close()
-
-        return var_list
-    # END: parse_vcf_file
 # END: class 'VCFData'
