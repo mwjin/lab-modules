0165ca4ceadc5d4755fec162a897be004d1856b6
Minwoo Jeong <jmw95217@gmail.com>
2018-01-20 10:14:11 +0900

    implement genome utilities

--- genome/genome_utils.py
+++ genome/genome_utils.py
@@ -14,10 +14,11 @@ from genome.fasta import Fasta
 
 GENOME = Fasta(GENOME_FILENAME)
 
+
 def reverse_complement(seq):
     base_to_comp = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A',
                     'a': 't', 'c': 'g', 'g': 'c', 't': 'a',
-                    'N': 'N', '.':'.'}
+                    'N': 'N', '.': '.'}
 
     comp_seq = ''
 
@@ -61,6 +62,37 @@ def get_chr_sizes():
         chr_to_size[chrID] = chr_size
 
     return chr_to_size
+
+
+def get_chr_bin_ranges(bin_size):
+    """
+    :param bin_size: (integer) the size of each bin you want
+    :return: a dictionary that maps chromosome ID to the list of tuples
+             that represent the ranges of bins in each chromosome
+
+             e.g. if a size of a chromosome is 2550 and the bin size is 1000,
+                  then the list of tuples of the chromosome will be [(0, 1000), (1000, 2000), (2000, 2550)].
+    """
+    chr_to_size = get_chr_sizes()
+    chr_to_bin_ranges = {}
+
+    for chrID in chr_to_size:
+        assert chrID not in chr_to_bin_ranges
+        chr_to_bin_ranges[chrID] = []
+
+        chr_size = chr_to_size[chrID]
+        bin_cnt = chr_to_size[chrID] / bin_size + 1
+
+        for i in range(bin_cnt):
+            bin_start_idx = i * bin_size
+            bin_end_idx = bin_start_idx + bin_size
+
+            if bin_end_idx > chr_size:
+                bin_end_idx = chr_size
+
+            chr_to_bin_ranges[chrID].append((bin_start_idx, bin_end_idx))
+
+    return chr_to_bin_ranges
 
 
 def main():
