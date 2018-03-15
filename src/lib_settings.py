139cb326e58227079d2dd79ed6f5ad2f8bb5e511
Minwoo Jeong <jmw95217@gmail.com>
2018-03-15 16:46:20 +0900

    correct the path of base directory

--- src/lib_settings.py
+++ src/lib_settings.py
@@ -3,6 +3,6 @@
 from os.path import dirname, abspath
 
 # paths for all the projects
-BASE_DIR = dirname(dirname(abspath(__file__)))  # the directory that contains all my projects
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))  # the directory that contains all my projects
 DATA_DIR = "%s/data" % BASE_DIR  # the directory that contains common data for my projects
 ROOT_DIR = dirname(BASE_DIR)
