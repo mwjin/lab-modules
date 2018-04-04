3252d9d3b7b84a278d0c0779e155a5ee33e82183
Minwoo Jeong <jmw95217@gmail.com>
2018-04-04 13:28:09 +0900

    add comments

--- lab/genome/utils.py
+++ lab/genome/utils.py
@@ -16,12 +16,23 @@ _GENOME = Fasta(GENOME_FILENAME)  # singleton
 
 
 def reverse_complement(seq):
+    """
+    :param seq: a string that consists of deoxy-nucleotides (A, T, G, C, ...)
+    :return: reverse complementary sequence of that input
+    """
     return _GENOME.reverse_complement(seq)
 
 # END: reverse_complement
 
 
 def get_seq(chrom, start, end, upper=True):
+    """
+    :param chrom: chromosome ID (e.g. chr1, chr2, ...)
+    :param start: a start position on the chromosome (0-based)
+    :param end: an end position on the chromosome
+    :param upper: if True, return a sequence which characters are all capitalized.
+    :return: the corresponding genome sequence
+    """
     start = int(start)
     end = int(end)
     seq = _GENOME.fetch_seq(chrom, start, end)
