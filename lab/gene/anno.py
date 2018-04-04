a00e57602f1d105dcad79bb765b2edf64f737230
Minwoo Jeong <jmw95217@gmail.com>
2018-04-04 12:17:07 +0900

    generalize the length of bit for the 'genic region value'

--- lab/gene/anno.py
+++ lab/gene/anno.py
@@ -8,7 +8,8 @@ GENIC_REGION_LIST = ['ORF', '5UTR', '3UTR', 'ncRNA_exonic', 'intronic', 'ncRNA_i
 
 """
 /* genic region value */
-The gene-based annotation of one nucleotide is represented as integer which bit length is 6.
+The gene-based annotation of one nucleotide is represented as integer which bit length is # of genic regions - 1
+('intergenic' is excluded).
 Each bit of the integer represents a boolean value for one genic region.
 
 1st bit: ORF (MSB)
@@ -22,8 +23,9 @@ For example, if a nucleotide is annotated as ORF and intron, an integer value th
 of the nucleotide will be 34 (0b100010). If the integer value is 0, it means that the nucleotide is intergenic.
 """
 
-_region_to_bit_pos = {genic_region: (i + 1) for i, genic_region in enumerate(GENIC_REGION_LIST)}
-_bit_pos_to_region = {(i + 1): genic_region for i, genic_region in enumerate(GENIC_REGION_LIST)}
+BIT_LEN = len(GENIC_REGION_LIST) - 1
+_REGION_TO_BIT_POS = {genic_region: (i + 1) for i, genic_region in enumerate(GENIC_REGION_LIST)}
+_BIT_POS_TO_REGION = {(i + 1): genic_region for i, genic_region in enumerate(GENIC_REGION_LIST)}
 
 
 def get_genic_region_val(genic_region_to_bool):
@@ -41,8 +43,8 @@ def get_genic_region_val(genic_region_to_bool):
 
         for genic_region in GENIC_REGION_LIST:
             if genic_region_to_bool[genic_region]:
-                bit_pos = _region_to_bit_pos[genic_region]
-                region_val += 2 ** (6 - bit_pos)
+                bit_pos = _REGION_TO_BIT_POS[genic_region]
+                region_val += 2 ** (BIT_LEN - bit_pos)
 
         assert region_val != 0
         return region_val
@@ -56,18 +58,18 @@ def parse_genic_region_val(region_val):
     :param region_val: an integer that represents a genic region value
     :return: a dictionary (key: genic region, value: boolean (whether the region ios used for the annotation)
     """
-    assert 0 <= region_val < 64  # the maximum bit length of the genic region value is 6
+    assert 0 <= region_val < (2 ** BIT_LEN)  # the maximum bit length of the genic region value is 6
 
     genic_region_dict = {genic_region: False for genic_region in GENIC_REGION_LIST}
 
     if region_val == 0:
         genic_region_dict['intergenic'] = True
     else:
-        for bit_pos in range(1, 7):
-            bit = int(region_val / (2 ** (6 - bit_pos))) % 2
+        for bit_pos in range(1, BIT_LEN + 1):
+            bit = int(region_val / (2 ** (BIT_LEN - bit_pos))) % 2
 
             if bit == 1:
-                genic_region = _bit_pos_to_region[bit_pos]
+                genic_region = _BIT_POS_TO_REGION[bit_pos]
                 genic_region_dict[genic_region] = True
 
     return genic_region_dict
