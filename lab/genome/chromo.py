d2e70b726e19201b4b33bc0fd15ea591b0730a9b
Minwoo Jeong <jmw95217@gmail.com>
2018-04-05 10:18:07 +0900

    change the struncture of the module

--- lab/genome/chromo.py
+++ lab/genome/chromo.py
@@ -6,58 +6,49 @@ Modules for the functions associated with the chromosome
 from lab.genome.settings import GENOME_FILENAME
 
 
-def get_chr_sizes():
-    """
-    :return: a dictionary that maps chromosome ID to the size of this chromosome
-    """
-    chr_to_size = {}
+_CHR_TO_SIZE = {}  # chromosome ID to its size
 
-    genome_idx_file = open('%s.fai' % GENOME_FILENAME, 'r')
-
-    for line in genome_idx_file.readlines():
+with open('%s.fai' % GENOME_FILENAME, 'r') as GENOME_IDX_FILE:
+    for line in GENOME_FILENAME.readlines():
         """
-        Column 1: chromosome ID e.g. chr1
-        Column 2: size of each chromosome without newline characters
-        Column 3: size of each chromosome with newline characters
-        Column 4: length of lines in each chromosome without newline characters
-        Column 5: length of lines in each chromosome with newline characters
+        Field 1: chromosome ID e.g. chr1
+        Field 2: size of each chromosome without newline characters
+        Field 3: size of each chromosome with newline characters
+        Field 4: length of lines in each chromosome without newline characters
+        Field 5: length of lines in each chromosome with newline characters
         """
         fields = line.split('\t')
-
-        chrom = fields[0]
-        chr_size = int(fields[1])
-
-        chr_to_size[chrom] = chr_size
-
-    return chr_to_size
+        _CHR_TO_SIZE[fields[0]] = int(fields[1])
 
 
-def get_chr_bin_ranges(bin_size):
+def get_chr_size(chrom):
     """
-    :param bin_size: (integer) the size of each bin you want
-    :return: a dictionary that maps chromosome ID to the list of tuples
-             that represent the ranges of bins in each chromosome
+    :param chrom: the chromosome ID (e.g. chr1, chr2, chr3, ..., chrX, chrY)
+    :return: the size of the chromosome
+    """
+    return _CHR_TO_SIZE[chrom]
+
+
+def bin_chr(chrom, bin_size):
+    """
+    :param chrom: the chromosome ID (e.g. chr1, chr2, chr3, ..., chrX, chrY)
+    :param bin_size: (integer) the size of each bin
+    :return: the list of tuples each of which represents the range (start, end) of each bin
 
              e.g. if a size of a chromosome is 2550 and the bin size is 1000,
                   then the list of tuples of the chromosome will be [(0, 1000), (1000, 2000), (2000, 2550)].
     """
-    chr_to_size = get_chr_sizes()
-    chr_to_bin_ranges = {}
+    chr_size = _CHR_TO_SIZE[chrom]
+    bin_cnt = int(chr_size / bin_size) + 1
+    chr_bins = []
 
-    for chrom in chr_to_size:
-        assert chrom not in chr_to_bin_ranges
-        chr_to_bin_ranges[chrom] = []
+    for i in range(bin_cnt):
+        bin_start_idx = i * bin_size
+        bin_end_idx = bin_start_idx + bin_size
 
-        chr_size = chr_to_size[chrom]
-        bin_cnt = int(chr_to_size[chrom] / bin_size) + 1
+        if bin_end_idx > chr_size:
+            bin_end_idx = chr_size
 
-        for i in range(bin_cnt):
-            bin_start_idx = i * bin_size
-            bin_end_idx = bin_start_idx + bin_size
+        chr_bins.append((bin_start_idx, bin_end_idx))
 
-            if bin_end_idx > chr_size:
-                bin_end_idx = chr_size
-
-            chr_to_bin_ranges[chrom].append((bin_start_idx, bin_end_idx))
-
-    return chr_to_bin_ranges
+    return chr_bins
