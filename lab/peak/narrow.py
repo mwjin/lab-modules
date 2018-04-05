6a8252805e2542967a42f1b0c2ce8d5a9fe3c66a
Minwoo Jeong <jmw95217@gmail.com>
2018-04-05 15:17:38 +0900

    implement the methods 'merge' and 'intersect'

--- lab/peak/narrow.py
+++ lab/peak/narrow.py
@@ -4,29 +4,33 @@ class NarrowPeak:
     """ The object of this class represents one entry of a file that has Narrow Peaks format """
     def __init__(self, *args, **kwargs):
         """
-        :param args: Its length must be 0 or 3. If the length is 3, args must consist of the following values
+        :param args: Its length must be 0, 3, or 4. If the length is 3 or 4, args must consist of the following values
             1. the chromosome ID (string)
             2. start position (integer)
             3. end position (integer)
+            4. strand (+ or -) (string) (if the length is 3, there is no input for strand)
             If the length is 0, default values will be entered.
 
         :param kwargs: The key values must be matched with one of the attributes in this class.
         """
         argc = len(args)
-        assert argc == 0 or argc == 3
+        assert argc == 0 or argc == 3 or argc == 4
 
-        if argc == 0:
-            self.chrom = None
-            self.start = 0  # 0-based
-            self.end = 0
-        else:
+        self.chrom = None
+        self.start = 0  # 0-based
+        self.end = 0
+        self.strand = '.'
+
+        if argc != 0:  # 3 or 4
             self.chrom = args[0]
             self.start = int(args[1])
             self.end = int(args[2])
 
+            if argc == 4:
+                self.strand = args[3]
+
         self.name = '.'
         self.score = '0'
-        self.strand = '.'
         self.sig_val = '0.0'
         self.p_val = '-1.0'
         self.q_val = '-1.0'
@@ -36,7 +40,7 @@ class NarrowPeak:
             if hasattr(self, attr):
                 setattr(self, attr, kwargs[attr])
             else:
-                raise AttributeError('Error: %s has no attribute named as %s.' % (self.__class__, attr))
+                raise AttributeError('Error: %s has no attribute named as %s.' % (self.__class__.__name__, attr))
 
     # END: __init__
 
@@ -54,6 +58,54 @@ class NarrowPeak:
         return self.start, self.end
 
     # END: the function 'get_position'
+
+    def merge(self, peak):
+        """
+        merge the position of this peak with the input peak and return the peak which has the merged position
+        :param peak: an object of the same class which chromosomal position and strand are same with this peak
+        :return: an object of the 'NarrowPeak' or None if there is a gap between the two peaks
+        """
+        assert peak.__class__.__name__ == self.__class__.__name__
+        assert peak.chrom == self.chrom
+        assert peak.strand == self.strand
+
+        pos_list = []
+
+        peak_start, peak_end = peak.get_position()
+        pos_list.append((peak_start, peak_end))
+        pos_list.append((self.start, self.end))
+        pos_list.sort(key=lambda pos: (pos[0], pos[1]))
+
+        if pos_list[1][0] - pos_list[0][1] > 0:
+            return None
+        else:
+            return NarrowPeak(self.chrom, pos_list[0][0], pos_list[1][1], self.strand)
+
+    # END: the function 'merge'
+
+    def intersect(self, peak):
+        """
+        intersect the position of this peak with the input peak and return the peak which has the intersected position
+        :param peak: an object of the same class which chromosomal position and strand are same with this peak
+        :return: an object of the 'NarrowPeak' or None if there is no overlap
+        """
+        assert peak.__class__.__name__ == self.__class__.__name__
+        assert peak.chrom == self.chrom
+        assert peak.strand == self.strand
+
+        pos_list = []
+
+        peak_start, peak_end = peak.get_position()
+        pos_list.append((peak_start, peak_end))
+        pos_list.append((self.start, self.end))
+        pos_list.sort(key=lambda pos: (pos[0], pos[1]))
+
+        if pos_list[1][0] - pos_list[0][1] >= 0:
+            return None
+        else:
+            return NarrowPeak(self.chrom, pos_list[1][0], pos_list[0][1], self.strand)
+
+    # END: the function 'intersect'
 
     def parse_peak_entry(self, peak_entry):
         """
