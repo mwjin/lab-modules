ee955787af28dc940fe0502c3c535f230cecf88b
Minwoo Jeong <jmw95217@gmail.com>
2018-04-06 15:18:19 +0900

    rename the variable

--- lab/genome/settings.py
+++ lab/genome/settings.py
@@ -2,4 +2,4 @@ from lab.settings import ROOT_DIR
 
 # settings for genome
 GENOME_VER = "hg19"
-GENOME_FILENAME = "{0}/ref_genome/{1}/{1}.fa".format(ROOT_DIR, GENOME_VER)
+GENOME_FILE_PATH = "{0}/ref_genome/{1}/{1}.fa".format(ROOT_DIR, GENOME_VER)
