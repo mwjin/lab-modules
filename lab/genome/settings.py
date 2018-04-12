8221971184a57d4c9d63dde517360f9b70d664ea
Minwoo Jeong <jmw95217@gmail.com>
2018-04-12 15:27:22 +0900

    correct the paths in settings

--- lab/genome/settings.py
+++ lab/genome/settings.py
@@ -1,7 +1,5 @@
-from lab.settings import ROOT_DIR
-
 __all__ = ['GENOME_VER', 'GENOME_FILE_PATH']
 
 # settings for genome
 GENOME_VER = "hg19"
-GENOME_FILE_PATH = "{0}/ref_genome/{1}/{1}.fa".format(ROOT_DIR, GENOME_VER)
+GENOME_FILE_PATH = "/extdata6/Minwoo/ref_genome/{0}/{0}.fa".format(GENOME_VER)
--- lab/settings.py
+++ lab/settings.py
@@ -5,5 +5,5 @@ from os.path import dirname, abspath
 __all__ = ['BASE_DIR', 'ROOT_DIR']
 
 # paths for all the projects
-BASE_DIR = dirname(dirname(dirname(abspath(__file__))))  # the directory that contains all the projects
+BASE_DIR = '/extdata6/Minwoo/projects'  # the directory that contains all the projects
 ROOT_DIR = dirname(BASE_DIR)
