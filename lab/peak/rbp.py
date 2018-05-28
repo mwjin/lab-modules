c3edfb24147e815c81c1ba23bb780c1c1c79c7ce
jeongmw <jmw95217@gmail.com>
2018-05-28 10:18:16 +0900

    fix the error to return NoneType

--- lab/peak/rbp.py
+++ lab/peak/rbp.py
@@ -56,7 +56,7 @@ class RBPPeak(NarrowPeak):
         """
         :return: a list of the positions (0-based) of all variants on this peak
         """
-        return list(self.var_pos_to_cnt.keys()).sort()
+        return sorted(list(self.var_pos_to_cnt.keys()))
 
     def get_var_cnt_in_pos(self, var_pos):
         """
