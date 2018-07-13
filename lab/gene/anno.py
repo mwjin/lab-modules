622660b6a8aa40ce3d8854832c11889b5c29a6cd
jeongmw <jmw95217@gmail.com>
2018-07-13 15:28:15 +0900

    rename: genic region value -> annotation value

--- lab/gene/anno.py
+++ lab/gene/anno.py
@@ -7,7 +7,7 @@ import sys
 from lab.utils import caller_file_and_line, eprint
 
 """
-/* genic region value */
+/* Annotation value */
 The gene-based annotation of one nucleotide is represented as integer which bit length is # of genic regions - 1
 ('intergenic' is excluded).
 Each bit of the integer represents a boolean value for one genic region.
@@ -22,7 +22,7 @@ Each bit of the integer represents a boolean value for one genic region.
 If the integer value is 0, it means that the nucleotide is intergenic.
 """
 
-__all__ = ['genic_region_list', 'get_genic_region_val', 'parse_genic_region_val']
+__all__ = ['genic_region_list', 'get_anno_priority', 'get_anno_val', 'parse_anno_val']
 
 # constants used in this module
 _GENIC_REGIONS = ['ORF', '5UTR', '3UTR', 'ncRNA_exonic',
@@ -46,9 +46,9 @@ def get_anno_priority(genic_region):
     return _REGION_TO_BIT_POS[genic_region]
 
 
-def get_genic_region_val(genic_region):
+def get_anno_val(genic_region):
     """
-    get the genic region value of the input genic region (string)
+    get the annotation value of the input genic region (string)
     :param genic_region: a string that represents the genic region
     :return: an integer that represents the region value of the input genic region
     """
@@ -60,34 +60,24 @@ def get_genic_region_val(genic_region):
         sys.exit()
 
 
-def parse_genic_region_val(region_val):
+def parse_anno_val(anno_val):
     """
-    parse the genic region value and get information which genic region is used for the annotation
-    :param region_val: an integer that represents a genic region value
+    parse the annotation value and get information which genic region is used for the annotation
+    :param anno_val: an integer
     :return: a dictionary (key: genic region, value: boolean (whether the region is used for the annotation)
     """
-    assert 0 <= region_val < (2 ** _BIT_LEN)  # the maximum bit length of the genic region value is 6
+    assert 0 <= anno_val < (2 ** _BIT_LEN)  # the maximum bit length of the genic region value is 6
 
-    genic_region_dict = {genic_region: False for genic_region in _GENIC_REGIONS}
+    anno_dict = {genic_region: False for genic_region in _GENIC_REGIONS}
 
-    if region_val == 0:
-        genic_region_dict['intergenic'] = True
+    if anno_val == 0:
+        anno_dict['intergenic'] = True
     else:
         for bit_pos in range(1, _BIT_LEN + 1):
-            bit = int(region_val / (2 ** (_BIT_LEN - bit_pos))) % 2
+            bit = int(anno_val / (2 ** (_BIT_LEN - bit_pos))) % 2
 
             if bit == 1:
                 genic_region = _BIT_POS_TO_REGION[bit_pos]
-                genic_region_dict[genic_region] = True
+                anno_dict[genic_region] = True
 
-    # deal with the case of splicing sites
-    if genic_region_dict['SS']:
-        genic_region_dict['SS-30nt'] = True
-
-    if genic_region_dict['SS-30nt']:
-        genic_region_dict['SS-50nt'] = True
-
-    if genic_region_dict['SS-50nt']:
-        genic_region_dict['intronic'] = True
-
-    return genic_region_dict
+    return anno_dict
