8bdececee8d354e362178d4ef077a609b61c1d65
jeongmw <jmw95217@gmail.com>
2018-09-28 12:16:35 +0900

    change the directory name: peak -> bed

--- lab/bed/varpeak.py
+++ lab/bed/varpeak.py
@@ -1,4 +1,4 @@
-from lab.peak.narrow import NarrowPeak
+from lab.bed.narrow import NarrowPeak
 from lab.gene.anno import genic_region_list, parse_anno_val, get_repr_anno
 
 __all__ = ['VarPeak']
@@ -11,12 +11,12 @@ class VarPeak(NarrowPeak):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # attributes for the variants on this peak
+        # attributes for the variants on this bed
         self.var_pos_to_cnt = {}  # positions of variants (0-based) to their counts
         self.var_pos_to_anno_val = {}  # positions of variants to their annotation values
         self.var_pos_to_genes = {}  # positions of variants to their associated genes (type: dictionary)
 
-        # attributes for the gene-based annotation of the peak
+        # attributes for the gene-based annotation of the bed
         self.anno_vals = []
         self.genic_region_to_size = {genic_region: 0 for genic_region in self._genic_regions}
         self.genic_region_to_var_cnt = {genic_region: 0 for genic_region in self._genic_regions}
@@ -25,14 +25,14 @@ class VarPeak(NarrowPeak):
 
     def combine(self, other):
         """
-        Combine the distribution of variants on other peak with this peak.
+        Combine the distribution of variants on other bed with this bed.
         :param other: an object of 'VarPeak'
         """
         assert other.__class__.__name__ == self.__class__.__name__
         assert other == self
 
         for var_pos in other.var_pos_to_cnt:
-            # combine the attributes for the variants on this peak
+            # combine the attributes for the variants on this bed
             var_cnt = other.get_var_cnt_in_pos(var_pos)
             anno_val = other.get_anno_val_in_pos(var_pos)
             genes = other.get_genes_in_pos(var_pos)
@@ -46,7 +46,7 @@ class VarPeak(NarrowPeak):
                 assert self.var_pos_to_genes[var_pos] == genes
                 self.var_pos_to_cnt[var_pos] += var_cnt
 
-            # combine the attributes for the gene-based annotation of this peak
+            # combine the attributes for the gene-based annotation of this bed
             anno_dict = parse_anno_val(anno_val)
             repr_is_multi, repr_genic_region = get_repr_anno(anno_val)
 
@@ -62,7 +62,7 @@ class VarPeak(NarrowPeak):
 
     def cut(self, start, end):
         """
-        cut the peak and return new object
+        cut the bed and return new object
         :param start: a start position of the new object
         :param end: an end position of the new object
         :return: a 'VarPeak' object
@@ -80,7 +80,7 @@ class VarPeak(NarrowPeak):
 
         for var_pos in var_pos_list:
             if start <= var_pos < end:
-                # make up the attributes for the variant distribution of this peak
+                # make up the attributes for the variant distribution of this bed
                 var_cnt = self.var_pos_to_cnt[var_pos]
                 var_anno_val = self.var_pos_to_anno_val[var_pos]
                 assoc_genes = self.var_pos_to_genes[var_pos]
@@ -89,7 +89,7 @@ class VarPeak(NarrowPeak):
                 cut_peak.var_pos_to_anno_val[var_pos] = var_anno_val
                 cut_peak.var_pos_to_genes[var_pos] = assoc_genes
 
-                # make up the attributes for the gene-based annotation of this peak
+                # make up the attributes for the gene-based annotation of this bed
                 anno_dict = parse_anno_val(var_anno_val)
                 repr_is_multi, repr_genic_region = get_repr_anno(var_anno_val)
 
@@ -113,7 +113,7 @@ class VarPeak(NarrowPeak):
     def get_genic_region_to_size(self, only_repr=False):
         """
         :param only_repr: if True, then return repr_genic_region_to_size
-        :return: a dictionary that documents the size of each genic region on this peak.
+        :return: a dictionary that documents the size of each genic region on this bed.
                  (key: a genic region, value: a size of the genic region (integer))
         """
         if only_repr:
@@ -124,7 +124,7 @@ class VarPeak(NarrowPeak):
     def get_genic_region_to_var_cnt(self, only_repr=False):
         """
         :param only_repr: if True, then return repr_genic_region_to_var_cnt
-        :return: a dictionary that documents the number of variants of each genic region on this peak.
+        :return: a dictionary that documents the number of variants of each genic region on this bed.
                  (key: a genic region, value: the number of variants on the genic region (integer))
         """
         if only_repr:
@@ -134,13 +134,13 @@ class VarPeak(NarrowPeak):
 
     def get_var_pos_list(self):
         """
-        :return: a list of the positions (0-based) of all variants on this peak
+        :return: a list of the positions (0-based) of all variants on this bed
         """
         return sorted(list(self.var_pos_to_cnt.keys()))
 
     def get_var_cnt(self):
         """
-        :return: the number of variants in this peak
+        :return: the number of variants in this bed
         """
         var_cnt = 0
 
@@ -151,7 +151,7 @@ class VarPeak(NarrowPeak):
 
     def get_var_cnt_in_pos(self, var_pos):
         """
-        :param var_pos: a variant position (0-based) inside the peak
+        :param var_pos: a variant position (0-based) inside the bed
         :return: the number of variants on the position
         """
         var_cnt = self.var_pos_to_cnt.get(var_pos)
@@ -161,7 +161,7 @@ class VarPeak(NarrowPeak):
 
     def get_anno_val_in_pos(self, var_pos):
         """
-        :param var_pos: a variant position (0-based) inside the peak
+        :param var_pos: a variant position (0-based) inside the bed
         :return: a genic region value of the position
         """
         anno_val = self.var_pos_to_anno_val.get(var_pos)
@@ -171,14 +171,14 @@ class VarPeak(NarrowPeak):
 
     def get_genes_in_pos(self, var_pos):
         """
-        :param var_pos: a variant position (0-based) inside the peak
+        :param var_pos: a variant position (0-based) inside the bed
         :return: a dictionary (mapping route: gene symbol -> ID -> genic region)
         """
         return self.var_pos_to_genes[var_pos]
 
     def get_anno_vals(self):
         """
-        Return the annotation values of this peak (each element corresponds to each nucleotide on this peak).
+        Return the annotation values of this bed (each element corresponds to each nucleotide on this bed).
         If it is not set, return an empty list.
         """
         return self.anno_vals
@@ -188,7 +188,7 @@ class VarPeak(NarrowPeak):
         This function enters the value of the attribute 'anno_vals' and
         makes up the 'genic_region_to_size' and 'repr_genic_region_to_size' attribute.
         * representative genic region: a genic region which has the highest priority among genic region candidates
-        :param anno_val_list: a list of anntation values corresponding to this peak (see gene.utils)
+        :param anno_val_list: a list of anntation values corresponding to this bed (see gene.utils)
         """
         assert len(anno_val_list) == self.get_size()
         self.anno_vals = anno_val_list
@@ -213,7 +213,7 @@ class VarPeak(NarrowPeak):
 
     def put_variant(self, variant):
         """
-        Make up the distribution of variants on this peak
+        Make up the distribution of variants on this bed
         :param variant: an object of the class 'VCFData'
         * representative genic region: a genic region which has the highest priority among genic region candidates
         """
