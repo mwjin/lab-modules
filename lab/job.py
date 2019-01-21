10cccbf360c1054c2251fc8b793a35a7a2035d98
Minwoo Jeong <jmw95217@gmail.com>
2019-01-21 18:31:56 +0900

    implement the function 'qsub_sge'

--- lab/job.py
+++ lab/job.py
@@ -4,26 +4,53 @@ This script includes 'Job' class and functions for job schedulers (SGE and PBS).
 * Original Author: Sukjun Kim
 """
 
+import os
+import sys
+import subprocess
+
 
 class Job:
     """
     # TODO: make explanation
     """
-    def __init__(self, **kwargs):
+    def __init__(self, name, **kwargs):
         self.id = None  # submission ID
-        self.name = kwargs.get('name', 'Anonymous')
-        self.queue = kwargs.get('queue', None)
-        self.cmd = ''
+        self.name = name
+        self.cmd = kwargs.get('cmd', '')
 
         # attributes for job dependency
         self.prior_jobs = []  # a list of prior jobs in execution than this job
-        self.hold_jid = kwargs.get('hold_jid', None)  # only used in SGE job scheduler
+        self.hold_jid = kwargs.get('hold_jid', None)  # regex for names of prior jobs (only used in SGE job scheduler)
         self.priority = kwargs.get('priority', 0)  # a priority to execute (for dependency, only used in PBS scheduler)
 
 
-def qsub_sge():
-    pass
+def qsub_sge(jobs, queue, log_dir):
+    """
+    Throw jobs using Sun Grid Engine (SGE) job scheduler
+    :param jobs: a list of 'Job' objects
+    :param queue: a queue name
+    :param log_dir: a directory of logs for the jobs
+    """
+    os.makedirs(log_dir, exist_ok=True)
+
+    for job in jobs:
+        log_path = '%s/%s.txt' % (log_dir, job.name)
+        qsub_cmd = 'qsub -cwd -j y -o %s -q %s -N %s' % (log_path, queue, job.name)
+
+        if job.hold_jid is not None:
+            qsub_cmd += ' -hold_jid %s' % job.hold_jid
+
+        echo_proc = subprocess.Popen(('echo', job.cmd), stdout=subprocess.PIPE)
+        subprocess.Popen(('qsub', '-cwd', '-j', 'y', '-o', log_path, '-q', queue, '-N', job.name),
+                         stdin=echo_proc.stdout, stdout=sys.stdout, stderr=sys.stderr)
+        echo_proc.wait()
 
 
-def qsub_pbs():
+def qsub_pbs(jobs, queue, log_dir):
+    """
+    Throw jobs using PBS job scheduler
+    :param jobs: a list of 'Job' objects
+    :param queue: a queue name
+    :param log_dir: a directory of logs for the jobs
+    """
     pass
